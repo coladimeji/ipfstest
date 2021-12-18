37df8f8fb3b5535ffb2b9817c2af5eed0ed1235c
coladimeji <coladimeji@gmail.com>
2021-12-18 13:24:49 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+const ipfsClient = require("ipfs-http-client");
+ 
+const ipfs = ipfsClient("http://localhost:8080/ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc");
+const ver = await ipfs.version();
+console.log ("IPFS Version=", ver);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <h1> My IPFS Test on React</h1>
     </div>
   );
 }
---
+++ src/ipfs.config.js
@@ -0,0 +1,13 @@
+import ipfsClient from "ipfs-http-Client"
+
+
+'ipfs config\
+--json \
+API : "AudioParam.HTTPHeaders.Access-Control-Allow-origin" \
+NETWORK : [https://gateway.pinata.cloud/ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc] '
+
+
+'ipfs config\
+--json \
+API : "AudioParam.HTTPHeaders.Access-Control-Allow-Methods" \
+'["PUT","GET","POST"] 
